615bd6327297e01a757ecc43519e1acc755a0b5a
Hidekazu Kondo <hide@MacBook.local>
2020-01-18 17:24:48 +0900

    Comment out comments

--- src/bridge/message-bridge-for-rn.ts
+++ src/bridge/message-bridge-for-rn.ts
@@ -21,7 +21,7 @@ export class MessageBridgeForRN extends MessageBridgeBase {
 
   public init() {
     this.setupCallbacks();
-    console.log('Alice : setupCallbacks');
+    // window.alert('Alice : setupCallbacks');
    }
 
   public injectWeb3Object() {
@@ -32,20 +32,18 @@ export class MessageBridgeForRN extends MessageBridgeBase {
     });
 
     window.web3 = new Web3(provider);
-    console.log('Alice : web3 injected');
+    // window.alert('Alice : web3 injected');
   }
 
   public injectEtheremObject() {
     window.ethereum = this._provider;
 
-    console.log('Alice : window.ethereum injected');
+    // window.alert('Alice : window.ethereum injected');
   }
 
   private postMessage(data, callback) {
     data['timestamp'] = Date.now();
-
-    window.alert('sendTransaction:' + JSON.stringify(data));
-
+    // window.console('Alice : sendTransaction:' + JSON.stringify(data));
     this._callbacks[data.timestamp] = callback;
     window.ReactNativeWebView.postMessage(JSON.stringify(data));
   }
@@ -54,6 +52,7 @@ export class MessageBridgeForRN extends MessageBridgeBase {
     window.alice = {
       callback: (response) => {
         const result = JSON.parse(response);
+        // window.console('Alice : call back of sendTransaction:' + result);
 
         if (this._callbacks[result.timestamp]) {
           this._callbacks[result.timestamp](result.data);
